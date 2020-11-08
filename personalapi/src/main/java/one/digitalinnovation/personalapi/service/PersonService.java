f9034acbb3bbbeca3bac778b030b4929096f44bd
Wilker Vinicios <willkervinicios@gmail.com>
2020-11-08 17:22:25 -0300

    Criado PersonDTO; Criado PersonMapper; Criado PhoneDTO; Alterado PersonController; Alterado PersonService; Alterado local de MessageResponseDTO; Adicionado mapstruct ao pom.xml;

--- personalapi/src/main/java/one/digitalinnovation/personalapi/service/PersonService.java
+++ personalapi/src/main/java/one/digitalinnovation/personalapi/service/PersonService.java
@@ -1,22 +1,25 @@
 package one.digitalinnovation.personalapi.service;
 
-import one.digitalinnovation.personalapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personalapi.dto.request.PersonDTO;
+import one.digitalinnovation.personalapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personalapi.entity.Person;
+import one.digitalinnovation.personalapi.mapper.PersonMapper;
 import one.digitalinnovation.personalapi.repository.PersonRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Pessoa criada com ID "+savedPerson.getId())
