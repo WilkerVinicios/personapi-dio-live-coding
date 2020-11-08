226dd39dc750da0584cd1047bdf1db52ada77f71
Wilker Vinicios <willkervinicios@gmail.com>
2020-11-08 18:47:21 -0300

    Criado PersonNotFoundException; Alterado PersonController; Alterado PersonService; Adicionado método Get por Id;

--- personalapi/src/main/java/one/digitalinnovation/personalapi/controller/PersonController.java
+++ personalapi/src/main/java/one/digitalinnovation/personalapi/controller/PersonController.java
@@ -2,6 +2,7 @@ package one.digitalinnovation.personalapi.controller;
 
 import one.digitalinnovation.personalapi.dto.request.PersonDTO;
 import one.digitalinnovation.personalapi.dto.response.MessageResponseDTO;
+import one.digitalinnovation.personalapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personalapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,5 +31,10 @@ public class PersonController {
     @GetMapping
     public List<PersonDTO> listAll(){
         return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findByID(id);
     }
 }
--- personalapi/src/main/java/one/digitalinnovation/personalapi/service/PersonService.java
+++ personalapi/src/main/java/one/digitalinnovation/personalapi/service/PersonService.java
@@ -3,11 +3,13 @@ package one.digitalinnovation.personalapi.service;
 import one.digitalinnovation.personalapi.dto.request.PersonDTO;
 import one.digitalinnovation.personalapi.dto.response.MessageResponseDTO;
 import one.digitalinnovation.personalapi.entity.Person;
+import one.digitalinnovation.personalapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personalapi.mapper.PersonMapper;
 import one.digitalinnovation.personalapi.repository.PersonRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,5 +35,11 @@ public class PersonService {
         List<Person> allPeople = personRepository.findAll();
         return allPeople.stream().map(personMapper::toDTO)
                 .collect(Collectors.toList());
+    }
+
+    public PersonDTO findByID(Long id) throws PersonNotFoundException {
+       Person person = personRepository.findById(id)
+               .orElseThrow(() -> new PersonNotFoundException(id));
+        return personMapper.toDTO(person);
     }
 }
